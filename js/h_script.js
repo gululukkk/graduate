9983fe3d363d1a0e64f5ab8f65cc745d1764c29e
long <1297368547@qq.com>
2016-11-20 22:00:07 +0800

    毕业设计第一次提交

---
+++ js/h_script.js
@@ -0,0 +1,49 @@
+var H = {
+	//导航条的滑块效果
+	slidebarEffect:(function() {
+		var $slideBar = $('#topnav .mainnav .slidebar');
+		var $navWraper = $('#topnav .mainnav');
+		var eventType = 'mouseover';
+		var slideBarInitalPosition = $slideBar.position();
+		var isReset = true;
+		
+		var getElemPosition = function($elem) {
+			var positionObj = $elem.Position();
+		}
+		
+		var setElemPosition = function($elem,positionObj) {
+			$elem.css(positionObj);
+		};
+		
+		var $subNav = $('#topnav .subnav');
+		var toggleShowStatus = function($elem,isShow) {
+			if (isShow === true) {
+				$elem.show();
+			} else {
+				$elem.hide();
+			}
+		};
+		var O = {
+			run:function() {
+				$navWraper.on(eventType,'.item',function() {
+					var targetPosition = $(this).position();
+					$slideBar.stop(true).animate(targetPosition,500);
+					var index = $(this).index();
+					var $currentSubNav = $subNav.eq(index);
+					toggleShowStatus($currentSubNav,true);
+					toggleShowStatus($currentSubNav.siblings('.subnav'),false);
+				});
+				if (isReset) {
+					$navWraper.parent().on('mouseleave',function() {
+						$slideBar.animate(slideBarInitalPosition);
+						toggleShowStatus($subNav,false);
+					});
+				}
+			},
+		};
+		return O;
+	})(),
+};
+H.slidebarEffect.run();
+
+$('#picture-show').slideBox();
